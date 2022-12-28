286aac1970e11b5046ae5168d3d2e12fd5813b2a
Luke Patrick <luke@routerchowder.com>
2022-12-27 20:22:06 -0600

    chore: hack together super basic apollo server implementation, stage for future awesome

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,48 @@
 import express, { Express, Request, Response } from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+// The GraphQL schema
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+
 const app: Express = express();
+const httpServer = http.createServer(app);
 const port = process.env.PORT || 3000;
+
+// Set up Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+await server.start();
+
+app.use('/graphql', cors(), bodyParser.json(), expressMiddleware(server));
 
 app.get('/', (_: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+// });
+await new Promise((resolve) => httpServer.listen({ port }, resolve));
+console.log(`🚀 Server ready at http://localhost:${port}`);
